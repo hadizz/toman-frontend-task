419e7cd0d28ed0db5022a1b080b196ea7995edd6
Hadi Zare <hadizareoriginal@gmail.com>
2024-12-23 13:58:03 +0330

    chore: scaffold 🐒

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div className='bg-zinc-950 dark:bg-white min-h-screen flex items-center justify-center'>
+        <h1 className='text-white'>Hello World</h1>
+        <Button>Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
