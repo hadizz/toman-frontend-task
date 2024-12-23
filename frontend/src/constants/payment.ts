36ce6f1dec5e26e08bbc29d2a8694d3ccf000e7e
Hadi Zare <hadizareoriginal@gmail.com>
2024-12-23 15:49:33 +0330

    chore(front): setup prettier 🎨

---
+++ frontend/src/constants/payment.ts
@@ -0,0 +1,9 @@
+export enum PaymentType {
+  SALARY = 'salary',
+  BONUS = 'bonus',
+  COMMISSION = 'commission',
+  TRANSPORTATION = 'transportation',
+  OVERTIME = 'overtime',
+}
+
+export const paymentTypes = Object.values(PaymentType)
